d3bab44150e83ee992a1e089fdd8917d649cc1ac
satriawanbasri <satriawan.basri@gmail.com>
2024-12-28 12:40:23 +0800

    -

---
+++ src/MyComponent.js
@@ -0,0 +1,5 @@
+import { Button } from 'react-native'
+
+const MyComponent = ({ text }) => <Button title={text}></Button>
+
+export default MyComponent
--- src/string.js
+++
@@ -1,5 +0,0 @@
-// src/string.js
-const toUpperCase = str => str.toUpperCase() + '-->5'
-const toLowerCase = str => str.toLowerCase()
-
-module.exports = { toUpperCase, toLowerCase }
